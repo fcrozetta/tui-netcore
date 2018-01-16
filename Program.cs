0b9f3768662d1f746b24e68f5a697bb4ac216de5
Fernando Crozetta <fernando@czetta.com>
2018-01-16 18:09:13 -0200

    drawYesNo Partial

--- Program.cs
+++ Program.cs
@@ -7,8 +7,12 @@ namespace tui_netcore
     {
         static void Main(string[] args)
         {
-            Tui t = new Tui();
-            t.TestSize(Tui.ColorSchema.System); 
+            Tui t = new Tui(50,25,5,3);
+            t.Title = "Sample Window";
+            t.Body = "This is a really long description that should break lines somewhere in the middle of the text. Maybe here, maybe there";
+            // t.TestSize(Tui.ColorSchema.System);
+            t.DrawYesNo(Tui.ColorSchema.System);
+            Console.ReadLine();
         }
     }
 }
--- TUI/Tui.cs
+++ TUI/Tui.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace tui_netcore.TUI
 {
@@ -6,6 +8,11 @@ namespace tui_netcore.TUI
     {
         public int Width { get; set; }
         public int Height { get; set; }
+        public int PosLeft { get; set; }
+        public int PosTop { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public char AnswerChar { get; set; }
         public char HorizontalChar { get; set; }
         public char VerticalChar { get; set; }
         public char TopLeftChar { get; set; }
@@ -13,6 +20,10 @@ namespace tui_netcore.TUI
         public char BottomLeftChar { get; set; }
         public char BottomRightChar { get; set; }
         public char IntersectionChar { get; set; }
+        public char EmptyChar { get; set; }
+        public int MarginLeft { get; set; }
+        public int MarginTop { get; set; }
+
         public enum ColorSchema
         {
             Regular,
@@ -23,10 +34,13 @@ namespace tui_netcore.TUI
 
         }
 
-        public Tui (int width = 100, int height = 80)
+        public Tui (int width = 100, int height = 80,int posLeft=0,int posTop=0)
         {
             Width = width;
             Height = height;
+            PosTop = 0;
+            PosLeft = 0;
+            AnswerChar= '>';
             HorizontalChar = '-';
             VerticalChar = '|';
             TopLeftChar = '+';
@@ -34,7 +48,11 @@ namespace tui_netcore.TUI
             BottomLeftChar = '+';
             BottomRightChar = '+';
             IntersectionChar = '+';
-            Console.Clear();
+            EmptyChar = ' ';
+            Title = null;
+            Body = null;
+            MarginLeft = 4;
+            MarginTop = 2;
         }
 
         private void setColorSchema(ColorSchema color)
@@ -82,18 +100,131 @@ namespace tui_netcore.TUI
             }
         }
 
-        public void MainFrame(ColorSchema schema = ColorSchema.Regular){
-            setColorSchema(schema);
-            for (int i = 0; i < Height; i++)
+        private void drawTitle()
+        {
+            Console.SetCursorPosition((Width / 2) - (Title.Length / 2), PosTop);
+            Console.Write(Title);
+        }
+
+        private void drawBody()
+        {
+            int usableSpace = Width - (2 * MarginLeft);
+            if (Body.Length <= usableSpace)
             {
-                for (int j = 0; j < Width; j++)
+                Console.SetCursorPosition((PosLeft + MarginLeft), (PosTop + MarginTop));
+                System.Console.Write(Body);
+            }
+            else
+            {
+                //* TODO: Improve the way the split is done to stay inside the box
+                int tmpLine = 0;
+                StringBuilder tmpText = new StringBuilder(usableSpace, usableSpace);
+                string[] tmpBody = Body.Split(" ");
+                foreach (string s in tmpBody)
                 {
-                    if ((i == 0) || (i == Height-1) )
+                    try
                     {
-                        Console.SetCursorPosition(j,i);
+                        tmpText.Append(s);
+                        tmpText.Append(" ");
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.SetCursorPosition((PosLeft + MarginLeft), (PosTop + MarginTop + tmpLine));
+                        System.Console.Write(tmpText);
+                        tmpLine++;
+                        tmpText.Clear();
                     }
                 }
+                Console.SetCursorPosition((PosLeft + MarginLeft), (PosTop + MarginTop + tmpLine));
+                System.Console.Write(tmpText);
             }
+
         }
+
+        /// <summary>
+        /// This Frame wraps the entire screen, printing spaces inside.
+        /// </summary>
+        /// <param name="schema">Color Scheme to be used</param>
+        public void Draw(ColorSchema schema = ColorSchema.Regular){
+            setColorSchema(schema);
+            for (int i = PosTop; i < Height; i++)
+            {
+                for (int j = PosLeft; j < Width; j++)
+                {
+                    char ToPrint = ' ';
+
+                    if ((i == 0) || (i == Height-1))
+                    {
+                        ToPrint = HorizontalChar;
+                    }else if((j == 0) || (j==Width-1)){
+                        ToPrint = VerticalChar;
+                    }else
+                    {
+                        ToPrint = EmptyChar;
+                    }
+                    Console.SetCursorPosition(j,i);
+                    Console.Write(ToPrint);
+                }
+            }
+            if (Title != null) drawTitle();
+            if (Body != null) drawBody();
+            setColorSchema(ColorSchema.Regular);
+        }
+
+        /// <summary>
+        /// This method Creates an input so the user can answer things
+        /// </summary>
+        /// <param name="schema"></param>
+        /// <returns>string containing the user answer</returns>
+        public string DrawInput(ColorSchema schema = ColorSchema.Regular){
+            Draw(schema);
+            setColorSchema(schema);
+            Console.SetCursorPosition((PosLeft + MarginLeft),(PosTop + Height - MarginTop));
+            Console.Write($"{AnswerChar} ");
+            string answer = Console.ReadLine();
+            setColorSchema(ColorSchema.Regular);
+            return answer;
+        }
+
+        public bool DrawYesNo(ColorSchema schema = ColorSchema.Regular,string txtYes = "Yes", string txtNo = "No", bool defaultAnswer = false){
+            Draw(schema);
+            setColorSchema(schema);
+            bool answer = defaultAnswer;
+            Console.SetCursorPosition((PosLeft + MarginLeft+1), (PosTop + Height - MarginTop));
+            Console.Write(txtYes);
+            Console.SetCursorPosition((PosLeft + Width - MarginLeft ), (PosTop + Height - MarginTop));
+            Console.Write(txtNo);
+            ConsoleKey keypress = Console.ReadKey().Key;
+            int CursorYes = PosLeft + MarginLeft;
+            int CursorNo = PosLeft + Width - MarginLeft;
+            while (keypress != ConsoleKey.Enter)
+            {
+                if (answer)
+                {
+                    Console.SetCursorPosition(CursorNo, (PosTop + Height - MarginTop));
+                    Console.Write(EmptyChar);
+                    Console.SetCursorPosition(CursorYes, (PosTop + Height - MarginTop) );
+                    answer = true;
+                }else
+                {
+                    Console.SetCursorPosition(CursorYes, (PosTop + Height - MarginTop) );
+                    Console.Write(EmptyChar);
+                    Console.SetCursorPosition(CursorNo, (PosTop + Height - MarginTop));
+                    answer = false;
+                }
+                Console.Write(AnswerChar);
+                if ((keypress == ConsoleKey.LeftArrow) || (keypress == ConsoleKey.RightArrow))
+                {
+                    answer = !answer;
+                }
+            }
+            setColorSchema(ColorSchema.Regular);
+            return answer;
+
+        }
+
+
+
+
     }
 }
